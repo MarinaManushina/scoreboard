78f2e1f57f7619f14376948b8788d09318042b01
MarinaManushina <122769289+MarinaManushina@users.noreply.github.com>
2023-03-17 21:40:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/scoreboard/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.scoreboard;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String Key = "COUNTER";
+    private static final String Key2 = "COUNTER2";
+    private Integer Russia = 0;
+    private Integer SA = 0;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(Key, Russia);
+        outState.putInt(Key2, SA);}
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onRestoreInstanceState (Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        Russia = savedInstanceState.getInt(Key);
+        SA = savedInstanceState.getInt(Key2);
+        TextView counterView = findViewById(R.id.Russia);
+        TextView countView = findViewById(R.id.SA);
+        counterView.setText(Russia.toString());
+        countView.setText(SA.toString());}
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void MCOne(View view) {
+        Russia++;
+        TextView counterView = findViewById(R.id.Russia);
+        counterView.setText(Russia.toString());
+    }
+    public void MCTwo(View view) {
+        SA++;
+        TextView countView = findViewById(R.id.SA);
+        countView.setText(SA.toString());
+    }
+    public void Delete(View view) {
+        Russia = 0;
+        SA = 0;
+        TextView counterView = findViewById(R.id.Russia);
+        TextView countView = findViewById(R.id.SA);
+        counterView.setText(Russia.toString());
+        countView.setText(SA.toString());}
+}
---
+++ app/src/main/java/com/example/scoreboard/NonNull.java
@@ -0,0 +1,4 @@
+package com.example.scoreboard;
+
+public @interface NonNull {
+}
